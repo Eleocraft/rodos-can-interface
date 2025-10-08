495dccb71769fff0aa8229ec5f3a67a0e4d6de8f
eleocraft <eliseo.cailloux@gmx.de>
2025-10-08 19:33:55 +0200

    adding can configuration for stm32f407vg

--- src/lib.rs
+++ src/lib.rs
@@ -15,8 +15,13 @@
 pub mod receiver;
 pub mod sender;
 
+#[cfg(feature = "stm32g0b1ke")]
 use embassy_stm32::can::{
     self, filter::ExtendedFilter, BufferedCan, CanConfigurator, RxBuf, TxBuf
+};
+#[cfg(feature = "stm32f407vg")]
+use embassy_stm32::can::{
+    self, filter::BankConfig, BufferedCan, Can, Fifo, RxBuf, TxBuf
 };
 use embedded_can::ExtendedId;
 use heapless::Vec;
@@ -30,10 +35,17 @@ const RODOS_CAN_ID: u8 = 0x1C;
 #[derive(Debug)]
 pub struct FiltersFullError;
 
+#[cfg(feature = "stm32g0b1ke")]
 /// Can peripheral in configuration stage
 pub struct ConfigPeriph<'d> {
     rodos_filters: Vec<ExtendedFilter, 8>,
     configurator: CanConfigurator<'d>
+}
+#[cfg(feature = "stm32f407vg")]
+/// Can peripheral in configuration stage
+pub struct ConfigPeriph<'d> {
+    rodos_filters: Vec<BankConfig, 28>,
+    configurator: Can<'d>
 }
 
 /// Can peripheral in split (buffered) state
@@ -55,6 +67,7 @@ pub struct RodosCanInterface<State> {
     device_id: u8,
 }
 
+#[cfg(feature = "stm32g0b1ke")]
 impl<'d> RodosCanInterface<ConfigPeriph<'d>> {
     /// # create an instance using a base can configurator
     ///
@@ -177,6 +190,115 @@ impl<'d> RodosCanInterface<ConfigPeriph<'d>> {
         }
     }
 }
+
+#[cfg(feature = "stm32f407vg")]
+impl<'d> RodosCanInterface<ConfigPeriph<'d>> {
+    /// # create an instance using a base can object
+    ///
+    /// this function takes a minimally configured CanConfigurator instance
+    /// as well as the rodos id this device will identify itself to other devices
+    /// to generate the Can simply provide a periph bus, can rx and tx pins and an interrupt reference
+    /// ```
+    /// Can::new(p.FDCAN1, p.PA11, p.PA12, Irqs);
+    /// ```
+    /// in principle the Can Interface can be put into active state and used
+    /// directly following this function call, however you won't be able to\
+    /// receive any messages without specifying at least one topic with
+    /// ```
+    /// pub fn add_receive_topic(&mut self, rodos_topic: u16, rodos_device_id: Option<u8>)
+    /// ```
+    pub fn new(
+        configurator: Can<'d>,
+        device_id: u8,
+    ) -> Self {
+        let rodos_filters = Vec::new();
+        Self {
+            peripheral: ConfigPeriph { rodos_filters, configurator },
+            device_id,
+        }
+    }
+    /// # add topic filter
+    ///
+    /// This function takes a rodos topic id (u16) as well as 
+    /// an optional device Id (u8) and adds them to the hardware
+    /// filter of receivable messages. If no device id is specified
+    /// the topic will be accepted from all devices on the bus.
+    /// You need to call this function at least once on the configurator
+    /// to be able to receive messages
+    pub fn add_receive_topic(&mut self, rodos_topic: u16, rodos_device_id: Option<u8>) -> Result<&mut Self, FiltersFullError> {
+        let can_topic_id: u32 =
+            (RODOS_CAN_ID as u32) << (16 + 8) | (rodos_topic as u32) << 8;
+        let filter: BankConfig = if let Some(device) = rodos_device_id {
+            [
+                can::filter::ListEntry32::data_frames_with_id(ExtendedId::new(can_topic_id | device as u32).unwrap()),
+                can::filter::ListEntry32::data_frames_with_id(ExtendedId::new(can_topic_id | device as u32).unwrap())
+            ].into()
+        } else {
+            can::filter::Mask32::frames_with_ext_id(
+                ExtendedId::new(can_topic_id).unwrap(),
+                ExtendedId::new(0xFF_FF_FF_00).unwrap()
+            ).into()
+        };
+        self.peripheral.rodos_filters.push(filter).map_err(|_| FiltersFullError)?;
+        Ok(self)
+    }
+    /// # allow receiving messages from all telestion topics.
+    ///
+    /// This is strongly discuraged in production,
+    /// as the RODOS gateway will likely flood the
+    /// can bus with irrelevant messages
+    pub fn allow_all_topics(&mut self) -> Result<&mut Self, FiltersFullError> {
+        let can_topic_id: u32 = (RODOS_CAN_ID as u32) << (8 * 3);
+
+        let filter = can::filter::Mask32::frames_with_ext_id(
+            ExtendedId::new(can_topic_id).unwrap(),
+            ExtendedId::new(0xFF_00_00_00).unwrap()
+        ).into();
+        self.peripheral.rodos_filters.push(filter).map_err(|_| FiltersFullError)?;
+        Ok(self)
+    }
+    /// # set can bitrate
+    ///
+    /// This function simply calls the set_bitrate function on the can configurator.
+    /// In principle it does not matter whether you use this function or simply call
+    /// set_bitrate on the configurator before passing it to ::new()
+    pub fn set_bitrate(&mut self, bitrate: u32) -> &mut Self {
+        self.peripheral.configurator.set_bitrate(bitrate);
+        self
+    }
+    /// # Activate the can transmitter for sending and receiving
+    ///
+    /// + The const parameter *NUMBER_OF_SOURCES* specifies the size of the map for
+    /// incoming can message sources. One "source" is one device sending on one topic.
+    /// As this is used to generate a hash map NUMBER_OF_SOURCES needs to be a power of 2
+    ///
+    /// + The const parameter *MAX_PACKET_LENGTH* specifies the size of the buffer allocated to each
+    /// source. as one RODOS can message contains 5 bytes of payload this should be a multiple of 5
+    pub async fn activate<const NUMBER_OF_SOURCES: usize, const MAX_PACKET_LENGTH: usize, const TX_BUF_SIZE: usize, const RX_BUF_SIZE: usize>(
+        mut self,
+        tx_buf: &'static mut TxBuf<TX_BUF_SIZE>,
+        rx_buf: &'static mut RxBuf<RX_BUF_SIZE>,
+    ) -> RodosCanInterface<ActivePeriph<'d, NUMBER_OF_SOURCES, MAX_PACKET_LENGTH, TX_BUF_SIZE, RX_BUF_SIZE>> {
+
+        for (i, bank) in self.peripheral.rodos_filters.into_iter().enumerate() {
+            self.peripheral.configurator.modify_filters().enable_bank(i as u8, Fifo::Fifo0, bank);
+        }
+
+        // initialize buffered can
+        self.peripheral.configurator.enable().await;
+        let interface = self.peripheral.configurator.buffered(
+            tx_buf,
+            rx_buf
+        );
+        let receiver = receiver::RodosCanReceiver::new(interface.reader());
+        let sender = sender::RodosCanSender::new(interface.writer(), self.device_id);
+        RodosCanInterface {
+            peripheral: ActivePeriph { interface, receiver, sender },
+            device_id: self.device_id,
+        }
+    }
+}
+
 impl<'d, const NUMBER_OF_SOURCES: usize, const MAX_PACKET_LENGTH: usize, const TX_BUF_SIZE: usize, const RX_BUF_SIZE: usize>
     RodosCanInterface<ActivePeriph<'d, NUMBER_OF_SOURCES, MAX_PACKET_LENGTH, TX_BUF_SIZE, RX_BUF_SIZE>> {
     /// # Split the configurator into a configured sender and receiver instance
